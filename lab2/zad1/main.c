52c11c5489ba94c9de5bb269f952dbe11ce1cb51
Remigiusz Babiarz <rbabiarz@student.agh.edu.pl>
2026-04-21 00:55:27 +0200

    small fixes

--- lab2/zad1/main.c
+++ lab2/zad1/main.c
@@ -22,18 +22,19 @@ void sig_unblock() {
   sigemptyset(&msk);
   sigaddset(&msk, SIGUSR1);
   sigset_t old;
-  sigprocmask(SIG_BLOCK, &msk, &old);
+  sigprocmask(SIG_UNBLOCK, &msk, &old);
 }
 void handler(int sig_no) {
-  printf("A signal? In my process? How peculiar! Moreover tis would seem it's "
-         "number is %d\n",
-         sig_no);
+  printf("Wywołano handler dla sygnału %d\n", sig_no);
 }
+
 void sig_handle() { signal(SIGUSR1, handler); }
 
 int main(int argc, char *argv[]) {
-  if (argc < 2)
+  if (argc < 2) {
+    fprintf(stderr, "Brak argumentu default/ignore/mask/handle\n");
     exit(0);
+  }
   if (strcmp(argv[1], "default") == 0)
     sig_default();
   if (strcmp(argv[1], "ignore") == 0)
@@ -43,22 +44,24 @@ int main(int argc, char *argv[]) {
   if (strcmp(argv[1], "handle") == 0)
     sig_handle();
 
-  printf("I am beggining loop de loop\n");
-  for (int i = 0; i < 20; i++) {
+  printf("%d rozpoczyna pętlę\n", getpid());
+  for (int i = 1; i <= 20; i++) {
     printf("%d\n", i);
     if (i == 5 || i == 15) {
-      printf("I must signal myself posthaste\n");
+      printf("Wysyłam sygnał USR1\n");
       raise(SIGUSR1);
     }
     if (i == 10) {
       sigset_t pending_sigs;
+      sigemptyset(&pending_sigs);
       sigpending(&pending_sigs);
       if (sigismember(&pending_sigs, SIGUSR1)) {
-        printf("Good golly! T'would seem my precious signal has been blocked! "
-               "I must inquire my trusty sig_unblock immediately!\n");
+        printf("Odblokowywuję USR1\n");
         sig_unblock();
       }
     }
+    sleep(1);
   }
+  printf("Pętla została wykonana w całości\n");
   return 0;
 }
